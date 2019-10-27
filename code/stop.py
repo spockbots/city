4ef32bddb9b51479a54edb3ed45a7ce863fbf9b0
Anthony Sherman <shermanw@indiana.edu>
2019-10-27 17:55:30 -0400

    New simple stop-all-motors code

--- code/stop.py
+++ code/stop.py
@@ -1,4 +1,12 @@
-import spockbots.robot as robot
+#!/usr/bin/env micropython
 
+# Simple program to just turn off all the motors
 
-robot.beep()
+from ev3dev2.motor import Motor, OUTPUT_A, OUTPUT_B, OUTPUT_C, OUTPUT_D
+
+motorlist = [OUTPUT_A, OUTPUT_B, OUTPUT_C, OUTPUT_D]
+for motorport in motorlist:
+    motor = Motor(motorport)
+    motor.off()
+    del motor
+
