349be47ebf99e89db077345a5c015104b79c1dc6
laszewsk <laszewski@gmail.com>
2019-11-03 14:27:26 -0500

    readd line follower

--- lego/m.py
+++ lego/m.py
@@ -6,11 +6,15 @@ robot.setup()
 
 print(robot)
 
-robot.forward(25, 20)
-robot.forward(25, -20)
-robot.forward(25, 20)
-robot.forward(-25, 20)
-robot.turn(25, 90)
-robot.turn(25, -90)
+#robot.forward(25, 20)
+#robot.forward(25, -20)
+#robot.forward(25, 20)
+#robot.forward(-25, 20)
+#robot.turn(25, 90)
+#robot.turn(25, -90)
+
+#robot.gotoblack(25, 3)
+
+robot.followline(speed=25, distance=30)
 
 time.sleep(1)
--- lego/spockbots/motor.py
+++ lego/spockbots/motor.py
@@ -3,12 +3,33 @@ from pybricks.ev3devices import Motor
 from pybricks.parameters import Port
 from pybricks.parameters import Stop, Direction
 import math
-
+from pybricks.ev3devices import ColorSensor
+import time
 
 #######################################################
 # Motor
 #######################################################
 
+colorsensor = [None,None,None,None,None]
+
+class SpockbotsColorSensor(object):
+
+    def __init__(self, port=3):
+        if port == 1:
+            self.sensor = ColorSensor(Port.S1)
+        elif port == 2:
+            self.sensor = ColorSensor(Port.S2)
+        elif port == 3:
+            self.sensor = ColorSensor(Port.S3)
+        elif port == 4:
+            self.sensor = ColorSensor(Port.S4)
+
+    def light(self):
+        return self.sensor.reflection()
+
+
+for i in [2,3,4]:
+    colorsensor[i] = SpockbotsColorSensor(port=i)
 
 class SpockbotsMotor(object):
 
@@ -55,6 +76,9 @@ class SpockbotsMotor(object):
         self.tank = DriveBase(self.left, self.right, self.diameter, self.axle_track)
 
         return self.left, self.right, self.tank
+
+    def light(self, port):
+        return colorsensor[port].light()
 
     def reset(self):
         self.left.reset_angle(0)
@@ -166,3 +190,82 @@ class SpockbotsMotor(object):
         while abs(self.left.angle()) < abs(a) or abs(self.right.angle()) < abs(a):
             pass
         self.stop()
+
+
+    def gotoblack(self, speed, port, black=10):
+        """
+        The robot moves to the black line while using the sensor on the given port
+
+        :param speed: The speed
+        :param port: The port 1,2,3,4
+        :param black: The value to stop
+        """
+        self.on(speed, 0)
+        while self.light(port)  > black:
+            pass
+        self.stop()
+
+
+    def gotowhite(self, speed, port, white=90):
+        """
+        The robot moves to the white line while using the sensor on the given port
+
+        :param speed: The speed
+        :param port: The port 1,2,3,4
+        :param white: The value to stop
+        """
+
+        self.on(speed, 0)
+        while self.light(port) < white:
+            pass
+        self.stop()
+
+
+    def followline(self,
+            speed=25,    # speed 0 - 100
+            distance=None, # distance in cm
+            t=None,
+            port=3,      # the port number we use to follow the line
+            right=True,  # the side on which to follow the line
+            black=0,     # minimal balck
+            white=100,   # maximal white
+            delta=-35,   # paramaters to control smoothness
+            factor=0.7): # parameters to control smoothness
+
+        if right:
+            f = - 1.0
+        else:
+            f = 1.0
+
+        if distance is not None:
+            distance = 10 * distance
+
+        current = time.time()  # the current time
+        if t is not None:
+            end_time = current + t  # the end time
+
+        self.reset()
+
+        while True:
+            value = self.light(port)  # get the light value
+
+            # correction = delta + (factor * value)  # calculate the correction for steering
+            correction = f * factor * (value + delta)
+            # correction = f * correction  # if we drive backwards negate the correction
+
+            self.on(speed, correction)  # switch the steering on with the given correction and speed
+
+            # if the time is used we set run to false once the end time is reached
+            # if the distance is greater than the position than the leave the
+            angle = self.left.angle()
+
+            traveled = self.angle_to_distance(angle)
+
+            current = time.time()  # measure the crurrent time
+            if t is not None and current > end_time:
+                break  # leave the loopK
+            if distance is not None and distance < traveled:
+                break  # leave the loop
+
+        self.stop()  # stop the robot
+
