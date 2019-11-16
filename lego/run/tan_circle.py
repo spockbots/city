2c8d86d1f9dfe817d245ffa71c8c096de09cc002
laszewsk <laszewski@gmail.com>
2019-11-15 20:25:19 -0500

    tan run

--- lego/run/tan_circle.py
+++ lego/run/tan_circle.py
@@ -18,46 +18,33 @@ def run_tan_circle():
 
     print(robot)
 
+    def white():
+        return robot.value(2) >= 90
     #
     # setup gyro
     #
     gyro = Gyro(robot)
     gyro.setup()
 
+    #drive to tan circle
     gyro.forward(30, 85)
-    robot.gotowhite(25, 2, 90)
-    gyro.forward(25, 5)
-    gyro.turn(10, 10)
+    gyro.turn(speed=25, degrees=-50, killtime=2)
 
-    robot.turntowhite(10, "left", 2, 90)
-    robot.turntoblack(10, "left", 2, 5)
-    robot.turntowhite(10, "right", 2, 90)
+    #place tan buildings
+    gyro.forward(20, 25)
 
-    robot.followline(speed=10, distance=15, port=2, right=True)
-    gyro.turn(speed=10, degrees=-85, killtime=2)
-    gyro.forward(10, 6)
+    #back up
+    robot.forward(20, -10)
 
-    robot.forward(10, -10)
-    gyro.turn(speed=10, degrees=100, killtime=2)
-    robot.forward(75, -140)
+    #turn home
+    gyro.turn(speed=25, degrees=50, killtime=2)
 
-    # move a long distance
-    # robot.forward(30,85)
-    # trun towards the white line
-    # robot.turn(25,-45)
-    # move a bit forward so we are not in the red circle as it colld be interpreted as white
-    # robot.forward(25,23)
-
-    # back up and leave the building
-    # robot.forward(25,-15)
-    # turn to home
-    # robot.turn(25, 15)
-    # go back to base
-    # robot.forward(100, -100)
-
+    #go home
+    robot.forward(100, -120)
 
 if __name__ == "__main__":
     time_start = time.time()
     run_tan_circle()
     time_end = time.time()
     print("Time:", time_end - time_start)
+
---
+++ other/tan_circle.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env pybricks-micropython
+
+from spockbots.motor import SpockbotsMotor
+from spockbots.gyro import SpockbotsGyro as Gyro
+import time
+from spockbots.output import led, PRINT
+
+
+def run_tan_circle():
+    """
+    Place the tan building in the circle
+    """
+    robot = SpockbotsMotor()
+    robot.debug = True
+
+    robot.setup()
+    robot.colorsensors.read()
+
+    print(robot)
+
+    def white():
+        return robot.value(2) >= 90
+    #
+    # setup gyro
+    #
+    gyro = Gyro(robot)
+    gyro.setup()
+
+    gyro.forward(30, 85)
+    robot.gotowhite(25, 2, 90)
+    gyro.forward(25, 5)
+    gyro.turn(10, 10)
+
+    robot.turntowhite(10, "left", 2, 90)
+    robot.turntoblack(10, "left", 2, 5)
+    robot.turntowhite(10, "right", 2, 90)
+
+    robot.followline(speed=10, distance=15, port=2, right=True)
+    gyro.turn(speed=10, degrees=-85, killtime=2)
+    gyro.forward(10, 6)
+
+    robot.forward(10, -10)
+    gyro.turn(speed=10, degrees=100, killtime=2)
+    robot.forward(75, -140)
+
+    # move a long distance
+    # robot.forward(30,85)
+    # trun towards the white line
+    # robot.turn(25,-45)
+    # move a bit forward so we are not in the red circle as it colld be interpreted as white
+    # robot.forward(25,23)
+
+    # back up and leave the building
+    # robot.forward(25,-15)
+    # turn to home
+    # robot.turn(25, 15)
+    # go back to base
+    # robot.forward(100, -100)
+
+    """
+if __name__ == "__main__":
+    time_start = time.time()
+    run_tan_circle()
+    time_end = time.time()
+    print("Time:", time_end - time_start)
+
+
+"""
